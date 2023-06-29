0d4046e8e58be8dc6ad5ea512239df49ec574387
Fabio Grieco <fabio.grieco@intre.it>
2023-06-29 17:52:06 +0200

    Rename function to fetch evolutions

--- src/components/EvolutionChain.tsx
+++ src/components/EvolutionChain.tsx
@@ -5,11 +5,11 @@ interface Props {
     pokemon: Pokemon
 }
 
-async function fetchPokemons(ids: number[]): Promise<Pokemon[]> {
+async function fetchEvolutions(ids: number[]): Promise<Pokemon[]> {
     return Promise.all(ids.map(id => fetch(`http://localhost:3001/pokemons/${id}`).then((res) => res.json())));
 }
 
 export default async function EvolutionChain({pokemon}: Props) {
-    const pokemons = await fetchPokemons(pokemon.evolutionChain)
+    const pokemons = await fetchEvolutions(pokemon.evolutionChain)
     return <PokemonGrid pokemons={pokemons}/>
 }
